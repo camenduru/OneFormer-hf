599758512dbf603cf426dce05d509be72e8cdc71
Jitesh Jain <jitesh.jj2@gmail.com>
2022-11-16 22:16:01 -0800

    Compile Worked, fix path data

--- oneformer/data/tokenizer.py
+++ oneformer/data/tokenizer.py
@@ -24,6 +24,8 @@
 # Modified by Jiarui Xu
 # -------------------------------------------------------------------------
 
+from StringIO import StringIO
+from urllib import urlopen
 import gzip
 import html
 import os
@@ -36,7 +38,10 @@ import torch
 
 @lru_cache()
 def default_bpe():
-    return os.path.join(os.path.dirname(os.path.abspath(__file__)), 'bpe_simple_vocab_16e6.txt.gz')
+    url = 'https://github.com/SHI-Labs/OneFormer/blob/main/oneformer/data/bpe_simple_vocab_16e6.txt.gz'
+    inmemory = StringIO(urlopen(url).read())
+    return inmemory
+    # return os.path.join(os.path.dirname(os.path.abspath(__file__)), 'bpe_simple_vocab_16e6.txt.gz')
 
 
 @lru_cache()
@@ -122,6 +127,9 @@ class SimpleTokenizer(object):
     def __init__(self, bpe_path: str = default_bpe()):
         self.byte_encoder = bytes_to_unicode()
         self.byte_decoder = {v: k for k, v in self.byte_encoder.items()}
+        
+        merges = gzip.GzipFile(fileobj=bpe_path, mode='rb')
+        
         merges = gzip.open(bpe_path).read().decode('utf-8').split('\n')
         merges = merges[1:49152 - 256 - 2 + 1]
         merges = [tuple(merge.split()) for merge in merges]
